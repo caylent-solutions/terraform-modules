48f2618d936d31ce79b212a15b388bca4f402c19
Lucas Valor <lucas.valor@caylent.com>
2025-11-14 11:27:51 -0300

    fixing tests

---
+++ providers/aws/primitives/sns-topic/examples/basic/outputs.tf
@@ -0,0 +1,7 @@
+output "topic_arn" {
+  value = module.sns_topic.topic_arn
+}
+
+output "topic_name" {
+  value = module.sns_topic.topic_name
+}
--- providers/aws/primitives/sns-topic/outputs.tf
+++ providers/aws/primitives/sns-topic/outputs.tf
@@ -2,3 +2,8 @@ output "topic_arn" {
   description = "ARN of the SNS topic"
   value       = aws_sns_topic.this.arn
 }
+
+output "topic_name" {
+  description = "Name of the SNS topic"
+  value       = aws_sns_topic.this.name
+}
