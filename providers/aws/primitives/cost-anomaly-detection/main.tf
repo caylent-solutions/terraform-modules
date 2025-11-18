bb98319978d5c9a400c6b95edd3064bfe0d2c65c
francisco.maestra@caylent.com <francisco.maestra@caylent.com>
2025-11-18 11:50:11 -0300

    apply tf formatting in a recursive fashion

--- providers/aws/primitives/cost-anomaly-detection/main.tf
+++ providers/aws/primitives/cost-anomaly-detection/main.tf
@@ -1,8 +1,8 @@
 # AWS Cost Anomaly Monitor
 resource "aws_ce_anomaly_monitor" "this" {
-  name                 = var.monitor_name != null ? var.monitor_name : "${var.name}-monitor"
-  monitor_type         = var.monitor_type
-  monitor_dimension    = var.monitor_type == "DIMENSIONAL" ? var.monitor_dimension : null
+  name                  = var.monitor_name != null ? var.monitor_name : "${var.name}-monitor"
+  monitor_type          = var.monitor_type
+  monitor_dimension     = var.monitor_type == "DIMENSIONAL" ? var.monitor_dimension : null
   monitor_specification = var.monitor_type == "CUSTOM" ? var.monitor_specification : null
 
   tags = var.tags
@@ -14,9 +14,9 @@ resource "aws_ce_anomaly_subscription" "this" {
 
   name      = var.subscription_name != null ? var.subscription_name : "${var.name}-subscription"
   frequency = var.subscription_frequency
-  
+
   monitor_arn_list = [aws_ce_anomaly_monitor.this.arn]
-  
+
   dynamic "subscriber" {
     for_each = var.subscribers
     content {
