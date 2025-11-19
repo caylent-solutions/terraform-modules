f3051b493ae4b462b9d969e2dcc337bc87587938
lvalor-caylent <lucas.valor@caylent.com>
2025-11-19 15:14:57 +0000

    fix: add AWS provider version constraint

--- providers/aws/primitives/sns-topic/versions.tf
+++ providers/aws/primitives/sns-topic/versions.tf
@@ -2,13 +2,9 @@ terraform {
   required_version = ">= 1.12.1"
 
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 2.0.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = ">= 0.7.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0.0"
     }
   }
 }
