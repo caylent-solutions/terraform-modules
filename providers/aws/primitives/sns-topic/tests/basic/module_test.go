f48a9c8390e099c8666177f71c1c3374eaa36140
lvalor-caylent <lucas.valor@caylent.com>
2025-11-19 18:37:58 +0000

    style: format Go test files

--- providers/aws/primitives/sns-topic/tests/basic/module_test.go
+++ providers/aws/primitives/sns-topic/tests/basic/module_test.go
@@ -1,23 +1,24 @@
 package basic_test
 
 import (
-    "testing"
-    "github.com/caylent-solutions/terraform-terratest-framework/pkg/testctx"
-    "github.com/gruntwork-io/terratest/modules/terraform"
-    "github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/caylent-solutions/terraform-terratest-framework/pkg/testctx"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBasicSNS(t *testing.T) {
-    t.Parallel()
+	t.Parallel()
 
-    ctx := testctx.RunSingleExample(t, "../../examples", "basic", testctx.TestConfig{
-        Name: "sns-basic-test",
-    })
+	ctx := testctx.RunSingleExample(t, "../../examples", "basic", testctx.TestConfig{
+		Name: "sns-basic-test",
+	})
 
-    topicArn := terraform.Output(t, ctx.Terraform, "topic_arn")
-    topicName := terraform.Output(t, ctx.Terraform, "topic_name")
+	topicArn := terraform.Output(t, ctx.Terraform, "topic_arn")
+	topicName := terraform.Output(t, ctx.Terraform, "topic_name")
 
-    assert.NotEmpty(t, topicArn)
-    assert.Contains(t, topicArn, "arn:aws:sns")
-    assert.NotEmpty(t, topicName)
+	assert.NotEmpty(t, topicArn)
+	assert.Contains(t, topicArn, "arn:aws:sns")
+	assert.NotEmpty(t, topicName)
 }
