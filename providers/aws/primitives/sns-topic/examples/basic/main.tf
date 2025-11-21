5bdf89a122df4d0b661f790b9226ee0743b2f6a9
lvalor-caylent <lucas.valor@caylent.com>
2025-11-21 13:28:38 +0000

    fix: pass kms_master_key_id to SNS topic module in basic example

--- providers/aws/primitives/sns-topic/examples/basic/main.tf
+++ providers/aws/primitives/sns-topic/examples/basic/main.tf
@@ -1,6 +1,7 @@
 module "sns_topic" {
   source = "../../"
 
-  name = var.name
-  tags = var.tags
+  name              = var.name
+  kms_master_key_id = var.kms_master_key_id
+  tags              = var.tags
 }
--- providers/aws/primitives/sns-topic/examples/basic/variables.tf
+++ providers/aws/primitives/sns-topic/examples/basic/variables.tf
@@ -8,3 +8,9 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "kms_master_key_id" {
+  description = "The ID or ARN of a customer-managed KMS key for SNS encryption."
+  type        = string
+  default     = null
+}
